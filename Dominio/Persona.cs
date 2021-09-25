b0612a71c19ce50b4533dc9c23d51b10bfcf20bf
Marcela Lasso <marcela.lasso@vixve.us>
2021-09-25 17:35:08 -0500

    n1

--- Dominio/Persona.cs
+++ Dominio/Persona.cs
@@ -6,7 +6,7 @@ namespace Dominio
         public string nombre { get; set; }
         public int edad { get; set; }
         public string telefono { get; set; }
-        public string direccion { get; set; }
+        public string direccion { get; set; } 
         
         
     }
